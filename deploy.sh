aad8008f1d0eed5000c16db9c82dbd4e4ede1e3b
Tomas Töws <tomas.toews96@gmx.de>
2022-03-27 01:22:09 +0100

    Changed username

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,23 @@
+#!/bin/bash
+
 project="carousels"
 container_port=8099
 
 echo "Starting deployment of $project"
 
 echo "Cleaning dist/ directory..."
-ssh root@tomastoews.de "rm -r -f -v /containers/$project && mkdir -p /containers/$project/dist"
+ssh tt@tomastoews.de "rm -r -f -v /containers/$project && mkdir -p /containers/$project/dist"
 echo "Cleaning done: ok"
 
 echo "Uploading dist/ files..."
-scp -r dist root@tomastoews.de:/containers/$project/;
+scp -r dist tt@tomastoews.de:/containers/$project/;
 echo "Upload done: ok"
 
 echo "Uploading dockerfile..."
-scp Dockerfile root@tomastoews.de:/containers/$project/
+scp Dockerfile tt@tomastoews.de:/containers/$project/
 echo "Upload done: ok"
 
-ssh root@tomastoews.de "cd /containers/scripts && sh ./build-image.sh $project && sh ./start.sh $project $container_port:80"
+ssh tt@tomastoews.de "cd /containers/scripts && sh ./build-image.sh $project && sh ./start.sh $project $container_port:80"
 
 echo "Image build done: ok"
 echo "Container start done: ok"
